b52c34c29aba1cceb2836d635d4d0bcd69c7f8a5
Christopher Ciufo <christopher.ciufo@gmail.com>
2016-01-16 21:59:25 -0500

    Completed the index page and added route for politician page.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var request = require('request');
 var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -10,14 +12,28 @@ app.set('view engine', 'handlebars');
 app.use('/bower', express.static('bower_components'));
 app.use('/public/assets', express.static('assets'));
 
+function errorLog (e) {
+  console.log('ERROR: ' + e.message);
+}
+
 app.get('/', function (req, res) {
-  res.render('index', { hello: 'hello world!'});
+  request('http://api.openparliament.ca/politicians/?limit=10', function (error, response, body) {
+    if (error) res.status(500).send('Something went wrong!');
+    body = JSON.parse(body);
+    res.render('index', { members: body.objects });
+  });
 });
 
 app.get('/politicians/:id', function (req, res) {
-  res.status(200);
+  console.log(req.params.id);
+  res.status(200).send('OK');
 });
 
-app.listen(3000, function () {
+// catch all non-registered routes as 404s
+app.get('*', function (req, res) {
+  res.status(404).send('404 Not Found lol.');
+});
+
+app.listen(port, function () {
   console.log('Listening on port 3000...');
 })
